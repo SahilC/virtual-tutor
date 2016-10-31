ed6bf97d8b50ebd78488125ac3769083b60e4f5e
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-10-31 21:53:45 +0530

    adding file for homography

---
+++ homography.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import sys
+
+if __name__ == '__main__':
+    try:
+        vidFile = cv2.VideoCapture("sample_videos/VID_20161024_165559.mp4")
+    except:
+        print "Problem opening input stream"
+        sys.exit(1)
+
+    if not vidFile.isOpened():
+        print "Capture stream not open"
+        sys.exit(1)
+    while ret:
+        cv2.imshow("frameWindow",gray)
+        cv2.waitKey(int(1/fps*1000))
+        ret, frame = vidFile.read()
