114c714ffa773c95c4d49badc60021c7cb549feb
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-11-24 19:44:58 +0530

    Refactoring code

--- src/white_key_detetion_working.py
+++ src/white_key_detetion_working.py
@@ -5,19 +5,33 @@ import math
 from extract_key import *
 from extract_calibration_frame import *
 
-element_small = cv2.getStructuringElement(cv2.MORPH_RECT,( 10,10 ),( 0, 0))
-def detect_white_keys(frame):
-    points = []
-    kernel_horizontal = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
-    temp = cv2.filter2D(frame,cv2.CV_8U,kernel_horizontal)
-    temp[temp > 80] = 0
-    temp[temp < 50] = 0
-    _,contours,_ = cv2.findContours(temp.copy(), 1, 2)
-    for cnt in contours:
-         x,y,w,h = cv2.boundingRect(cnt)
-         if w*h > 100 and w*h < 300 and w > h:
-             points.append((x,y,w,h))
-    return points
+def detect_keypress(frame, points, keymap):
+    cur_key_presses = set()
+    for (x,y,w,h) in points:
+        key = keymap[y,x]
+        if key != 0:
+            cur_key_presses.add(key)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),-1)
+        if len(prev_key_presses) < time_slice:
+            prev_key_presses.add(key)
+        else:
+            prev_key_presses  = set()
+
+def smart_threshold(im):
+    counts =  np.histogram(im,bins=range(0,256),range=(0,255),density=False)
+    counts = counts[0]
+    max = counts[-1]
+    count = 0
+    for i in xrange(len(counts)-2 ,0,-1):
+        if counts[i] - max > 100:
+            count += 1
+            if count > 1:
+                break
+            else:
+                im[im > i] = 0
+        max = counts[i]
+    im[im < i] = 0
+    return i
 
 if __name__ == '__main__':
     try:
@@ -31,82 +45,39 @@ if __name__ == '__main__':
         sys.exit(1)
 
     calibration_frame = extract_calibration_frame(vidFile)
-    # keymap = get_keymaps(calibration_frame)
     keymap = get_white_keymap(cv2.cvtColor(calibration_frame,cv2.COLOR_BGR2HSV))
-    # cv2.imshow("white",keymap)
     nFrames = int(vidFile.get(cv2.CAP_PROP_FRAME_COUNT))
     print "frame number: %s" %nFrames
     fps = vidFile.get(cv2.CAP_PROP_FPS)
     print "FPS value: %s" %fps
     ret, frame = vidFile.read()
-    counter = 0
     time_slice = 5
     frames = []
-    lower_white = np.array([0,20,30])
-    upper_white = np.array([20,255,255])
     prev_key_presses =  set()
     while ret:
         blur = cv2.GaussianBlur(frame,(0,0),3)
-        blur_hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
 
         blur = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
-        mask = cv2.inRange(blur_hsv,lower_white,upper_white)
         kernel_horizontal = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
         diff = cv2.filter2D(blur,cv2.CV_8U,kernel_horizontal)
 
         if len(frames) > 10:
             med = np.mean(frames,0)
             diff = np.abs(np.float64(diff) - np.float64(med))
-
             diff = np.uint8(diff)
 
-            # counts = np.bincount(diff.flatten())
-            counts =  np.histogram(diff,bins=range(0,256),range=(0,255),density=False)
-            counts = counts[0]
-            # print counts
-            max = counts[-1]
-            count = 0
-            for i in xrange(len(counts)-2 ,0,-1):
-                # print str(i) +" " + str(counts[i])
-                if counts[i] - max > 100:
-                    count += 1
-                    if count > 1:
-                        break
-                    else:
-                        diff[diff > i] = 0
-                max = counts[i]
-            # print 'WHoa'+str(i)
-            diff[diff < i] = 0
+            smart_threshold(diff)
 
-            diff[keymap == 0] = 0
-            # cv2.erode(diff,element_small)
             cv2.imshow("Keymap",diff)
-            # cv2.imshow("DIFF",diff)
-            counts = np.bincount(diff.flatten())
-            # diff[diff > 0] = 255
+
             points = []
             _,contours,_ = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
             for cnt in contours:
                  x,y,w,h = cv2.boundingRect(cnt)
-                 # if w*h > 100 and w*h < 500 and w > h:
                  if w*h > 50 and w > h:
                      points.append((x,y,w,h))
 
-
-            cur_key_presses = set()
-            for (x,y,w,h) in points:
-                key = keymap[y,x]
-                if key != 0:
-                    cur_key_presses.add(key)
-                    # print(key)
-                    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),-1)
-                    # cv2.rectangle(gray,(x,y),(x+w,y+h),255,-1)
-                    # cv2.circle(frame, (x,y), 5, (0,0,255), 3)
-                if len(prev_key_presses) < time_slice:
-                    prev_key_presses.add(key)
-                else:
-                    prev_key_presses  = set()
-
+            detect_keypress(frame, points, keymap)
 
             del frames[0]
         frames.append(diff)
